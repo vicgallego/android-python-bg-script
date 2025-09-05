a79444fbd2420b32d0ec03527fbbe9ef15841db4
vicgallego <victor_gallego1988@hotmail.com>
2025-09-05 16:17:56 +0200

    primera app básica en Kivy

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-print("Hola mundillo")
+from kivy.app import App
+from kivy.uix.label import Label 
+
+class MyApp(App):
+    def build(self):
+        return Label(text="Hola, esta es mi primera app")
+    
+if __name__ == "__main__":
+    MyApp().run()
